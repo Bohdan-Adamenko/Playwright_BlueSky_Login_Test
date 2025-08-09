35e0c245b70501ad1d459f05797f48e5492b0907
Bohdan-Adamenko <bohdan.adamenko.qa@gmail.com>
2025-08-09 14:52:20 +0200

    Update Playwright_BlueSky.py

--- Playwright_BlueSky.py
+++ Playwright_BlueSky.py
@@ -39,7 +39,7 @@ def run_test(page):
     overall_result = True
 
     # --- STEP 1: LOGIN ---
-    print("\n--- Login ---")
+    print("\n--- Assertions ---")
 
     assertion(page.goto, "https://bsky.app", 'Navigated to "bsky.app" homepage')
     assertion(page.click, 'button[aria-label="Sign in"]', 'Button "Sign In" clicked')
@@ -48,14 +48,11 @@ def run_test(page):
     assertion(page.click, 'button[aria-label="Next"]', 'Button "Next" clicked')
 
     # --- STEP 2: LOGIN VERIFICATION ---
-    print("\n--- Login verification ---")
+    print("\n--- Verifications ---")
 
-    if not verification(page, 'button[aria-label="Compose new post"]', 'Compose post'):
-        overall_result = False
-    if not verification(page, 'div[data-testid="homeScreenFeedTabs-Discover"]', 'Discover'):
-        overall_result = False
-    if not verification(page, 'button[aria-label="Find people to follow"]', 'Find people to follow'):
-        overall_result = False
+    overall_result &= verification(page, 'button[aria-label="Compose new post"]', 'Compose post')
+    overall_result &= verification(page, 'div[data-testid="homeScreenFeedTabs-Discover"]', 'Discover')
+    overall_result &= verification(page, 'button[aria-label="Find people to follow"]', 'Find people to follow')
 
     return overall_result
 
@@ -69,6 +66,8 @@ def main():
             viewport={"width": 1920, "height": 1080}
         )
         page = context.new_page()
+
+        final_status = False
 
         try:
             final_status = run_test(page)
@@ -98,4 +97,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
